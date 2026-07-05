a50b424b4c9f565a654a14ac8246c7d3ec24d934
memtx <kaja.hosnedl@seznam.cz>
2026-07-05 19:41:44 +0200

    everything reworked but now mostly fine

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Basic updatable Github Pages webpage 
 
 Currently HEAVILY WIP
+
+The data update process is done in this non-ideal way to be relatively easily updatable and still work on Github Pages, which don't really support APIs.
--- index.js
+++ index.js
@@ -1,1 +1,157 @@
+let el_categories = document.getElementById("categories");
+let el_statsTitle = document.getElementById("statisticsTitle");
+let el_statistics = document.getElementById("statistics");
+let el_allTeams   = document.getElementById("allTeams");
+
+//==== funkce ====
+
+let sortedTeams = [];
+let sortPosition = {};
+let navOpen = false;
+
+function strMax(a, b)
+{
+    if(a > b)
+        return a
+    else
+        return b
+}
+
+function loadLeaderboard(category="Vše", sorted = false)
+{
+    let sortCounter = {}
+            
+
+    for(let category of data.kategorie)
+    {
+        sortCounter[category] = 1;
+        sortPosition[category] = {}
+    }
+
+    for(let team of sortedTeams)
+    {
+        sortPosition[team.kategorie][team.jmeno] = sortCounter[team.kategorie];
+        sortCounter[team.kategorie]++;
+    }
+
+    let teams;
+    if(sorted)
+        teams = sortedTeams;
+    else 
+        teams = data.teamy;
+
+    el_statistics.innerHTML = `
+        <tr>
+            <th>Tým</th>
+            <th>LP</th>
+            <th>PP</th>
+            <th>Výsledný čas</th>
+            <th>Pozice v kategorii</th>
+        </tr>`;
+     
+
+    firstTimelessFound = false;
+
+    for(let team of teams)
+    {
+        
+        if(team.kategorie == category || category === "Vše")
+        {
+            let row = document.createElement("tr");
+            let name = document.createElement("td");
+            name.innerText = team.jmeno;
+            row.appendChild(name);
+            let lp = document.createElement("td");
+            lp.innerText = team.levy;
+            row.appendChild(lp);
+            let pp = document.createElement("td");
+            pp.innerText = team.pravy;
+            row.appendChild(pp);
+            let final = document.createElement("td");
+            final.innerText = strMax(team.levy, team.pravy);
+            row.appendChild(final);
+            
+            let position = sortPosition[team.kategorie][team.jmeno];
+            let el_position = document.createElement("td");
+
+            if(team.levy.length > 0)
+            {
+                el_position.innerText = position;
+                
+
+                switch(position)
+                {
+                    case 1:
+                        row.classList.add("statsFirst")
+                        break;
+                    case 2:
+                        row.classList.add("statsSecond")
+                        break;
+                    case 3:
+                        row.classList.add("statsThird")
+                        break;
+                }
+            }
+            row.appendChild(el_position);
+            
+            if(firstTimelessFound == false && team.levy.length == 0)
+            {
+                row.classList.add("statsHighlight")
+                firstTimelessFound = true;
+            }
+            el_statistics.appendChild(row);
+        }
+    }
+}
+
+function setActiveButton(btn)
+{
+    let parent = btn.parentElement;
+    for(let child of parent.children)
+        child.classList.remove("active");   
+   
+    btn.classList.add("active");
+}
+
+function timeComp(a, b)
+{
+    if(a.pravy.length == 0) return 1; 
+    if(b.pravy.length == 0) return -1; 
+    
+    return (strMax(a.levy, a.pravy) > strMax(b.levy, b.pravy));
+}
+//==== inicializace ====
+
+//proměnná data je načtena v html headeru z parsedData.js
+
+document.getElementById("poharTitul").innerText = data.soutezNazev;
+document.getElementById("poharDatum").innerText = data.soutezDatum;
+
+sortedTeams = data.teamy.toSorted(timeComp)
+
+let currentCategory = data.kategorie[0];
+
+let categoryVse = document.createElement("button");
+categoryVse.innerText = "Vše";
+categoryVse.classList.add(["active"]);  
+categoryVse.onclick = ()=>{loadLeaderboard(); setActiveButton(categoryVse);}
+el_categories.appendChild(categoryVse);
+
+let loopCounter = 0;
+for(let category of data.kategorie)
+{
+    let el = document.createElement("button");
+    el.innerText = data.kategorie[loopCounter];
+
+    const temp = data.kategorie[loopCounter]
+    el.onclick = ()=> {loadLeaderboard(temp); setActiveButton(el); }
+
+    el_categories.appendChild(el);
+    loopCounter++;
+} 
+/*
+appendAllStatistics(data.kategorie);
+loadTeams();
+*/
+loadLeaderboard();
 
