64241349fdccc2ad18395063a15f7cc9a51e26fb
Manulynx <kaja.hosnedl@seznam.cz>
2026-07-10 15:55:28 +0200

    app website update

--- soutez/parsedData.js
+++ soutez/parsedData.js
@@ -1,7 +1,7 @@
 let data = 
 {
   "soutezNazev": "Pohár Ukázkové Soutěže 2026",
-  "soutezDatum": "23.07.2026 17:00:13",
+  "soutezDatum": "23.07.2026 17:00:00",
   "kategorie": [
     "Muži",
     "Ženy"
