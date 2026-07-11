a1ae1cbed8826b664cbd9df42af4fa61d55376ed
Manulynx <kaja.hosnedl@seznam.cz>
2026-07-11 18:46:39 +0200

    app website update

---
+++ archiv/PoharUkazkoveSouteze2026/parsedData.js
@@ -0,0 +1,71 @@
+let data = 
+{
+  "soutezNazev": "Pohár Ukázkové Soutěže 2026",
+  "soutezDatum": "23.07.2026 17:00:00",
+  "kategorie": [
+    "Muži",
+    "Ženy"
+  ],
+  "teamy": [
+    {
+      "jmeno": "Skupina (Ženy)",
+      "kategorie": "Ženy",
+      "levy": "00:14.154",
+      "pravy": "00:16.134"
+    },
+    {
+      "jmeno": "Skupina",
+      "kategorie": "Muži",
+      "levy": "00:13.134",
+      "pravy": "00:15.134"
+    },
+    {
+      "jmeno": "Tým A",
+      "kategorie": "Muži",
+      "levy": "00:45.134",
+      "pravy": "01:14.136"
+    },
+    {
+      "jmeno": "Tým B",
+      "kategorie": "Muži",
+      "levy": "NP",
+      "pravy": "NP"
+    },
+    {
+      "jmeno": "Družstvo",
+      "kategorie": "Muži",
+      "levy": "00:24.134",
+      "pravy": "00:24.136"
+    },
+    {
+      "jmeno": "Seskupení A",
+      "kategorie": "Muži",
+      "levy": "00:26.432",
+      "pravy": "00:23.570"
+    },
+    {
+      "jmeno": "Seskupení B",
+      "kategorie": "Muži",
+      "levy": "NP",
+      "pravy": "NP"
+    },
+    {
+      "jmeno": "Seskupení (Ženy)",
+      "kategorie": "Ženy",
+      "levy": "",
+      "pravy": ""
+    },
+    {
+      "jmeno": "Družina",
+      "kategorie": "Muži",
+      "levy": "",
+      "pravy": ""
+    },
+    {
+      "jmeno": "Družstvo (Ženy)",
+      "kategorie": "Ženy",
+      "levy": "",
+      "pravy": ""
+    }
+  ]
+}
--- archiv/archivData.js
+++ archiv/archivData.js
@@ -1,2 +1,3 @@
 let archiv = [
+{"jmeno":"Pohár Ukázkové Soutěže 2026","cesta":"PoharUkazkoveSouteze2026/index.html"}
 ]
