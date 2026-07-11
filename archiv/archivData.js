abb792d198175fd334e17b4840b04a6354fc5f30
Manulynx <kaja.hosnedl@seznam.cz>
2026-07-11 18:45:25 +0200

    normalizace názvů

--- archiv/archivData.js
+++ archiv/archivData.js
@@ -1,4 +1,2 @@
 let archiv = [
-{"jmeno":"Pohár špatného Javascriptu 2026","cesta":"PoharspatnehoJavascriptu2026/index.html"},
-{"jmeno":"Pohár Ukázkové Soutěže 2026","cesta":"PoharUkazkoveSouteze2026/index.html"}
 ]
